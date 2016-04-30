9d81cdb389f1f3ee224d194a20f3d19af644ad8e
Mika Koivusaari <mika.koivusaari@gmail.com>
2016-04-30 23:04:35 +0300

    Added some comments and cleaned code.

--- mysql_to_graphite.py
+++ mysql_to_graphite.py
@@ -3,6 +3,15 @@
 
 #
 #
+#Graphite config is stored in the following table
+#mysql> desc graphite;
+#+--------------+---------------------+------+-----+---------+-------+
+#| Field        | Type                | Null | Key | Default | Extra |
+#+--------------+---------------------+------+-----+---------+-------+
+#| sensorid     | bigint(20) unsigned | NO   | PRI | NULL    |       |
+#| graphitepath | varchar(200)        | NO   |     | NULL    |       |
+#| formula      | varchar(4000)       | YES  |     | NULL    |       |
+#+--------------+---------------------+------+-----+---------+-------+
 
 
 from pymysqlreplication import BinLogStreamReader
@@ -71,57 +80,52 @@ def main():
   graphiteConfig = readGraphiteConfig()
 
   try:
-    print "alku"
+    print "Start"
     sock = socket.socket()
     sock.connect((CARBON_SERVER, CARBON_PORT))
     print 'Carbon socket opened.'
-    print 'replicationlogfile', repLogFile
-    print 'replicationlogposition', repLogPosition
     stream = BinLogStreamReader(
         connection_settings=MYSQL_SETTINGS,
-        server_id=2,
+        server_id=2, #server id needs to be unique
         only_events=[WriteRowsEvent,DeleteRowsEvent,UpdateRowsEvent],
         blocking=True,
         log_file=repLogFile,
         log_pos=repLogPosition,
         resume_stream=False if repLogPosition==None else True)
-    print "stream avattu"
+    print "Binlog stream opened"
 
     for binlogevent in stream:
-#      print stream.log_file, stream.log_pos
+      #put replication log file and position in variables so we can save them later
       repLogFile = stream.log_file
       repLogPosition = stream.log_pos
-#      print "%s:%s:" % (binlogevent.schema, binlogevent.table)
+      #also check for changes in graphite configuration and read again if needed
       if binlogevent.schema == "weather" and binlogevent.table == "graphite":
         graphiteConfig = readGraphiteConfig()
+      #this is the data we are interested in
       if binlogevent.schema == "weather" and binlogevent.table == "data":
-#      prefix = "%s:%s:" % (binlogevent.schema, binlogevent.table)
 
         for row in binlogevent.rows:
+          #we only care about inserts
           if isinstance(binlogevent, WriteRowsEvent):
             vals = row["values"]
+            #check if the sensor is one that we have configuration for
             if vals["sensorid"] in graphiteConfig:
               conf = graphiteConfig[vals["sensorid"]]
               value = float(vals["value"])
+              #do a conversion if needed
               if conf["formula"]!=None and conf["formula"]!="":
                 value=eval(conf["formula"], {"__builtins__": {}}, {"value":value,"round":round})
-              #path value unix_timestamp
-              #print type(vals["time"])
-              #print dir(vals["time"])
-              #message = '%s %f %s\n' % (conf["graphitepath"], value, calendar.timegm(vals["time"].timetuple()))
-              #_time = vals["time"]
-              #print _time.hour, _time.tzinfo
-              #message = '%s %f %s\n' % (conf["graphitepath"], value, time.mktime((_time.year, _time.month, _time.day, _time.hour, _time.minute, _time.second, -1, -1, -1)))
+              #construc the message and send it to carbon
               message = '%s %f %d\n' % (conf["graphitepath"], value, round((vals["time"] - _EPOCH).total_seconds()))
               sock.sendall(message)
-              #print message
-              #print type(vals["time"])
               print str(vals["sensorid"]), str(vals["time"]), str(value)
               print message
 
   except KeyboardInterrupt:
+    #close open connections
     stream.close()
     sock.close()
+    #save replication log position
     repLogConfig.set('replicationlog','file',repLogFile)
     repLogConfig.set('replicationlog','position',str(repLogPosition))
     with open('replogposition.ini', 'w') as f:
